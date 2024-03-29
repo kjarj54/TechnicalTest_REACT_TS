43864236d3d69694c2e7550852091cab1a8a68a2
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-03-28 21:55:06 -0600

    Update App.tsx

--- pruebatecnica/src/App.tsx
+++ pruebatecnica/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import { UsersLists } from "./components/UsersList";
 import { User } from "./types";
@@ -7,7 +7,9 @@ function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [showColors, setShowColors] = useState(false);
   const [sortByContry, setSortByContry] = useState(false);
+  const [filteredCountry, setFilteredCountry] = useState<string | null>(null);
   const originalUsers = useRef<User[]>([]);
+
   const toggleColors = () => {
     setShowColors(!showColors);
   };
@@ -19,13 +21,11 @@ function App() {
   const handleDelete = (email: string) => {
     const filteredUsers = users.filter((user) => user.email !== email);
     setUsers(filteredUsers);
-    
   };
 
-  const handleReset = () =>  {
-    setUsers(originalUsers.current)
-
-  }
+  const handleReset = () => {
+    setUsers(originalUsers.current);
+  };
 
   useEffect(() => {
     fetch("https://randomuser.me/api?results=100")
@@ -39,11 +39,23 @@ function App() {
       });
   }, []);
 
-  const sortedUsers = sortByContry
-    ? users.toSorted((a, b) => {
-        return a.location.country.localeCompare(b.location.country);
-      })
-    : users;
+  const filteredUsers =useMemo(() =>{
+   return typeof filteredCountry === "string" && filteredCountry.length > 0
+      ? users.filter((user) => {
+          return user.location.country
+            .toLowerCase()
+            .includes(filteredCountry.toLowerCase());
+        })
+      : users;
+  }, [users, filteredCountry])
+
+  const sortedUsers = useMemo(() => {
+    return sortByContry
+      ? filteredUsers.toSorted((a, b) =>
+          a.location.country.localeCompare(b.location.country)
+        )
+      : filteredUsers;
+  }, [filteredUsers, sortByContry]);
 
   return (
     <>
@@ -53,9 +65,13 @@ function App() {
         <button onClick={toggleSortByCountry}>
           {sortByContry ? "No ordenar por pais" : "Ordenar por pais"}
         </button>
-        <button onClick={handleReset}>
-          Resetear estado
-        </button>
+        <button onClick={handleReset}>Resetear estado</button>
+        <input
+          placeholder="Filtra por pais"
+          onChange={(e) => {
+            setFilteredCountry(e.target.value);
+          }}
+        />
       </header>
       <main>
         <UsersLists
